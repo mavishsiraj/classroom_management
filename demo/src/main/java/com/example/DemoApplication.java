a1261a4bb300a3dc3cdf738cf6715224a82b5933
taiba <mavishsiraj1@gmail.com>
2025-01-09 03:23:40 +0530

    fixed

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -1,10 +1,9 @@
-package com.com.example;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
 public class DemoApplication {
 
     public static void main(String[] args) {
